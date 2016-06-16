40da7c3a6e8b8ab15eeb33c847231890907bcde3
Rohan Y. Shah <rohanyshah@fb.com>
2016-06-16 14:36:53 -0700

    Initial Commit

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Rohan Y. Shah on 6/15/16.
+//  Copyright © 2016 Rohan Y. Shah. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
